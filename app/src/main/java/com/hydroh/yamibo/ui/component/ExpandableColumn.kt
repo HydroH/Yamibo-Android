5449e108b8b984ffacd96253878fae9606afe984
HydroH <iodizon@163.com>
2022-11-22 00:39:21 +0800

    将FoldableSectionGroup替换为通用的ExpandableColumn。

--- app/src/main/java/com/hydroh/yamibo/ui/component/ExpandableColumn.kt
+++ app/src/main/java/com/hydroh/yamibo/ui/component/ExpandableColumn.kt
@@ -6,40 +6,35 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.hydroh.yamibo.model.SectionGroup
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun FoldableSectionGroup(
-    sectionGroup: SectionGroup,
-    onExpandedChange: (Boolean) -> Unit,
+fun ExpandableColumn(
+    label: String,
     modifier: Modifier = Modifier,
+    initialExpanded: Boolean = true,
+    expanded: Boolean = true,
+    onExpandedChange: (Boolean) -> Unit,
+    content: @Composable () -> Unit,
 ) {
     Column(
         modifier = modifier.clickable {
-            onExpandedChange(sectionGroup.expanded)
+            onExpandedChange(expanded)
         },
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(text = sectionGroup.name)
+        Text(text = label)
         AnimatedVisibility(
-            initiallyVisible = sectionGroup.initialExpanded,
-            visible = sectionGroup.expanded,
+            initiallyVisible = initialExpanded,
+            visible = expanded,
             enter = expandVertically(),
             exit = shrinkVertically(),
         ) {
-            sectionGroup.SectionList.map {
-                // SectionItem
-                Card() {
-                    Text(text = it.name)
-                    Text(text = it.desc)
-                }
-            }
+            content()
         }
     }
 }
--- app/src/main/java/com/hydroh/yamibo/ui/home/HomeNode.kt
+++ app/src/main/java/com/hydroh/yamibo/ui/home/HomeNode.kt
@@ -3,6 +3,8 @@ package com.hydroh.yamibo.ui.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
@@ -11,7 +13,7 @@ import androidx.compose.ui.unit.dp
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 import com.bumble.appyx.navmodel.backstack.BackStack
-import com.hydroh.yamibo.ui.component.FoldableSectionGroup
+import com.hydroh.yamibo.ui.component.ExpandableColumn
 import com.hydroh.yamibo.ui.navigation.RootNode
 
 class HomeNode(
@@ -45,12 +47,22 @@ fun HomeScreen(
         LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
             uiState.sectionGroups.map {
                 item {
-                    FoldableSectionGroup(
-                        sectionGroup = it,
+                    ExpandableColumn(
+                        label = it.name,
+                        initialExpanded = it.initialExpanded,
+                        expanded = it.expanded,
                         onExpandedChange = { expanded ->
                             it.expanded = !expanded
                         },
-                    )
+                    ) {
+                        it.SectionList.map {
+                            // SectionItem
+                            Card() {
+                                Text(text = it.name)
+                                Text(text = it.desc)
+                            }
+                        }
+                    }
                 }
             }
         }
